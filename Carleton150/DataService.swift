91ff6860f16d28ce5e6dca1119d2d76d7cbe3b83
chetaldrich <chetaldrich@gmail.com>
2015-11-10 11:18:09 -0600

    Error handle for requests
    Before now, there wasn't correct error handling for the optionals
    returned from the JSON objects that were being given from the server.
    If the server is not on, this caused the app to crash. That is now
    fixed, along with a quirk where arrays are call by value in swift, and
    thus couldn't be edited in an outside context inside a callback. For
    now, I'm using a container class (which uses call by reference) to
    handle this until I find a more elegant solution.

--- Carleton150/DataService.swift
+++ Carleton150/DataService.swift
@@ -24,35 +24,46 @@ final class DataService {
                     "lng" : location.longitude
                 ],
                 "radius": 250
-            ],
-            "timespan": [
-                "startTime":"",
-                "endTime":""
             ]
         ]
         
+        let geofences: Geofences = Geofences()
         let postEndpoint: String = "https://carl.localtunnel.me/geofences"
+        
         Alamofire.request(.POST, postEndpoint, parameters: parameters, encoding: .JSON).responseJSON() {
             (request, response, result) in
             
-            let json = JSON(result.value!)
-            let answer = json["content"]
-            var answerArray:[(name: String, radius: Int, CLLcoord: CLLocationCoordinate2D)] = []
-            for i in 0 ..< answer.count {
-                //print(answer[i]["name"])
-                var geofence = answer[i]["geofence"]
-                var location = geofence["location"]
-                var latitude = location["lat"]
-                var longitude = location["lng"]
-                answerArray.append((name: answer[i]["name"].string!, radius: Int(answer[i]["geofence"]["radius"].string!)!, CLLcoord: CLLocationCoordinate2D(latitude: latitude.double!,longitude: longitude.double!)))
+            if let result = result.value {
+                let json = JSON(result)
+                if let answer = json["content"].array {
+                    for i in 0 ..< answer.count {
+                        let location = answer[i]["geofence"]["location"]
+                        let fenceName = answer[i]["name"].string!
+                        let rad = Int(answer[i]["geofence"]["radius"].string!)!
+                        let center = CLLocationCoordinate2D(latitude: location["lat"].double!,longitude: location["long"].double!)
+                        geofences.addNewGeofence((name: fenceName, radius: rad, center: center ))
+                    }
+                }
             }
-            print(answerArray)
         }
-
-        
-        return [("hello", 1, CLLocationCoordinate2D(latitude: 15.2, longitude: 15.1))]
+        return geofences.getGeofences()
     }
     
 }
 
-
+final class Geofences {
+    var geofenceArray: [(name: String, radius: Int, center: CLLocationCoordinate2D)]
+    
+    init () {
+        self.geofenceArray = []
+    }
+    
+    func addNewGeofence(element: (name: String, radius: Int, center: CLLocationCoordinate2D)) {
+       self.geofenceArray.append(element)
+    }
+    
+    func getGeofences() -> [(name: String, radius: Int, center: CLLocationCoordinate2D)] {
+        return self.geofenceArray
+    }
+    
+}
