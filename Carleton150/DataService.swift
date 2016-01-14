492d233ec2a0dab4f07c44936ff08a9a56e022ca
chetaldrich <chetaldrich@gmail.com>
2016-01-13 20:57:04 -0600

    Events query completed in DataService
    Now are able to query the server for events on
    the Carleton calendar.

--- Carleton150/DataService.swift
+++ Carleton150/DataService.swift
@@ -86,7 +86,6 @@ final class DataService {
                     for i in 0 ..< answer.count {
                         let location = answer[i]["geofence"]["location"]
                         let fenceName = answer[i]["name"].string!
-                        //print(answer[i]["geofence"]["radius"].string!)
                         let rad = answer[i]["geofence"]["radius"].int!
                         let center = CLLocationCoordinate2D(latitude: location["lat"].double!,longitude: location["lng"].double!)
                         final_result.append((name: fenceName, radius: rad, center: center))
@@ -103,4 +102,65 @@ final class DataService {
         }
     }
     
+    /**
+        Request events for the calendar.
+     
+        - Parameters:
+            - completion: function that will perform the behavior
+                          that you want given a list with all the events
+                          from the server.
+     */
+    class func requestEvents(date: NSDate?, limit: Int, completion:
+        (success: Bool, result: [Dictionary<String, String>]?) -> Void) {
+            
+        let dateFormatter : NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+           
+        var dateString : String = ""
+        if let desiredDate = date {
+            dateString = dateFormatter.stringFromDate(desiredDate)
+        } else {
+            let todaysDate = NSDate()
+            dateString = dateFormatter.stringFromDate(todaysDate)
+        }
+
+        let parameters = [
+            "startTime": dateString,
+            "limit": limit
+        ]
+        
+        let postEndpoint: String = "https://carl150.carleton.edu/events"
+        
+        Alamofire.request(.POST, postEndpoint, parameters: (parameters as! [String : AnyObject]), encoding: .JSON).responseJSON() {
+            (request, response, result) in
+            
+            var events : [Dictionary<String, String>] = []
+            
+            if let result = result.value {
+                let json = JSON(result)
+                if let answer = json["content"].array {
+                    for i in 0 ..< answer.count {
+                        let title = answer[i]["title"].string!
+                        let description = answer[i]["description"].string!
+                        let location = answer[i]["location"].string!
+                        let startTime = answer[i]["startTime"].string!
+                        let duration = answer[i]["duration"].string!
+                        let event = ["title": title,
+                                     "description": description,
+                                     "location": location,
+                                     "startTime": startTime,
+                                     "duration": duration]
+                        events.append(event)
+                    }
+                    completion(success: true, result: events)
+                } else {
+                    print("No results were found.")
+                    completion(success: false, result: nil)
+                }
+            } else {
+                print("Connection to server failed.")
+                completion(success: false, result: nil)
+            }
+        }
+    }
 }
