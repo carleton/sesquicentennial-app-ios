5f5f4ba36864f70be0bd6ef3d23318ca303bf305
chetaldrich <chetaldrich@gmail.com>
2016-01-19 13:57:54 -0600

    Build DataService query for quest data
    Now the app can get and use server quests as Quest objects.

--- Carleton150/Quest.swift
+++ Carleton150/Quest.swift
@@ -8,10 +8,12 @@ class Quest: NSObject {
     var wayPoints: [WayPoint]
     var name: String
     var questDescription: String
+    var completionMessage: String
     
-    init (wayPoints: [WayPoint], name: String, description: String) {
+    init (wayPoints: [WayPoint], name: String, description: String, completionMessage: String) {
         self.wayPoints = wayPoints
         self.name = name
         self.questDescription = description
+        self.completionMessage = completionMessage
     }
 }
