1be528ad18465856bb8e1f59b5d3e388d451d8c4
chetaldrich <chetaldrich@gmail.com>
2016-01-29 01:19:56 -0600

    Document the CalendarCell
    Some of the animation logic definitely merits an explanation before I
    forget what all of it does, so this seemed prudent.

--- Carleton150/CalendarCell.swift
+++ Carleton150/CalendarCell.swift
@@ -5,13 +5,37 @@
 import UIKit
 
 class CalendarCell: UICollectionViewCell {
-    
+   
+    /**
+        The background view for the calendar cell
+     */
     @IBOutlet weak var EventImageView: UIImageView!
-    @IBOutlet weak var imageCoverView: UIView!
-    @IBOutlet weak var eventTitle: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
     
+    /**
+        The view providing a darkening filter as the 
+        cell goes out of focus.
+     */
+    @IBOutlet weak var imageCoverView: UIView!
+    
+    /**
+        The title of the event.
+     */
+    @IBOutlet weak var eventTitle: UILabel!
+    
+    /**
+        The location of the event.
+     */
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    /**
+        The starting time of the event.
+     */
+    @IBOutlet weak var timeLabel: UILabel!
+   
+    /**
+        Sets the EventImageView to reflect the image
+        set when setting the currentImage property.
+     */
     var currentImage: UIImage? {
         didSet {
             if let currentImage = currentImage {
@@ -19,24 +43,31 @@ class CalendarCell: UICollectionViewCell {
             }
         }
     }
-    
+   
+    /**
+        Whenever the layout gets updated (which occurs as the user scrolls and invalidates
+        the current layout), updates to each individual cell occur here.
+     */
     override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
         super.applyLayoutAttributes(layoutAttributes)
-        
+       
+        // set constants for the standard vs. featured height to determine the delta for 
+        // the continuous animated transition.
         let standardHeight = CalendarLayoutConstants.Cell.standardHeight
         let featuredHeight = CalendarLayoutConstants.Cell.featuredHeight
         
         let delta = 1 - ((featuredHeight - CGRectGetHeight(frame)) / (featuredHeight - standardHeight))
-        
+       
+        // Set the amount of fade for the darkening view on top of each cell.
         let minAlpha: CGFloat = 0.3
         let maxAlpha: CGFloat = 0.65
         imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
        
-        // scale the title as the cell scales
+        // Scale the title as the cell scales
         let scale = max(delta, 0.75)
         eventTitle.transform = CGAffineTransformMakeScale(scale, scale)
        
-        // set time and location to fade in as the cell scales
+        // Set time and location to fade in as the cell scales
         locationLabel.alpha = delta
         timeLabel.alpha = delta
     }
