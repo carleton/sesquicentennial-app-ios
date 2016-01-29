f77eb476e23086d5c99bed9f890a7e44a92acb48
chetaldrich <chetaldrich@gmail.com>
2016-01-29 01:10:32 -0600

    Update documentation for CalendarViewController

--- Carleton150/CalendarViewController.swift
+++ Carleton150/CalendarViewController.swift
@@ -10,26 +10,35 @@ class CalendarViewController: UICollectionViewController {
     var eventImages: [UIImage] = []
     var tableLimit : Int!
     
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return UIStatusBarStyle.LightContent
-    }
-    
+    /**
+        Upon load of this view, load the calendar and adjust the 
+        collection cells for the calendar.
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // display the Carleton logo on the view
         Utils.showLogo(self)
+       
+        // gets the calendar data
         getCalendar(20, date: NSDate())
         
         // stop the navigation bar from covering the calendar content
         self.navigationController!.navigationBar.translucent = false;
 
+        // set the view's background colors
         view.backgroundColor = UIColor(red: 252, green: 212, blue: 80, alpha: 1.0)
         collectionView!.backgroundColor = UIColor(red: 224, green: 224, blue: 224, alpha: 1.0)
         
         // set the deceleration rate for the event cell snap
         collectionView!.decelerationRate = UIScrollViewDecelerationRateFast
     }
-   
-    // TODO: make this method get the images
+    
+    /**
+        Gets the image backgrounds for the calendar.
+     
+        - Returns: A list of UIImage backgrounds to place in the collection view.
+     */
     func getEventImages() -> [UIImage] {
         for i in 1 ..< 11 {
             let eventImage = UIImage(named: "Event-" + String(i))
@@ -40,6 +49,16 @@ class CalendarViewController: UICollectionViewController {
         return eventImages
     }
     
+    /**
+        Pulls the calendar data from the server, and loads the collection to
+        place the data into the views.
+     
+        - Parameters:
+            - limit:      A hard limit on the amount of quests returned
+                          by the server.
+     
+            - date:       The earliest date from which to get data.
+     */
     func getCalendar(limit: Int, date: NSDate?) {
         
         self.tableLimit = limit
@@ -59,14 +78,36 @@ class CalendarViewController: UICollectionViewController {
         }
     }
     
+    /**
+        Determines the number of sections in the calendar collection view.
+     
+        - Parameters:
+            - collectionView: The collection view being used for the calendar.
+     */
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
 
+    /**
+        Determines the number of cells in the calendar collection view.
+     
+        - Parameters:
+            - collectionView:         The collection view being used for the calendar.
+     
+            - numberOfItemsInSection: The number of items in the calendar.
+     */
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return schedule.count
     }
     
+    /**
+        Animates a section of the calendar to focus at the top of the page on tap.
+     
+        - Parameters:
+            - collectionView: The collection view being used for the calendar.
+     
+            - indexPath:      The index of the cell that was clicked.
+     */
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let layout = collectionViewLayout as! CalendarLayout
         let offset = layout.dragOffset * CGFloat(indexPath.item)
@@ -75,6 +116,16 @@ class CalendarViewController: UICollectionViewController {
         }
     }
     
+    /**
+        Fills each section of the calendar with data as they are loaded into the view.
+     
+        - Parameters:
+            - collectionView: The collection view being used for the calendar.
+     
+            - indexPath:      The index of the current cell.
+     
+        - Returns: A built calendar cell.
+     */
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CalendarCell", forIndexPath: indexPath) as! CalendarCell
         let images = getEventImages()
