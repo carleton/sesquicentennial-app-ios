f7d66531b755457413d98c127526da9a7a2cc9fb
chetaldrich <chetaldrich@gmail.com>
2016-02-18 02:51:22 -0600

    Allow image selection
    Now, upon clicking the "upload an image" button, it brings up the photo
    library and you can pick an image. Text fields still need to be able to
    dismiss keyboards, and there should be some confirmation on the screen
    that the image has actually been selected and saved.

--- Carleton150/MemoryUploadView.swift
+++ Carleton150/MemoryUploadView.swift
@@ -2,16 +2,45 @@
 //  MemoryUploadView.swift
 //  Carleton150
 
-class MemoryUploadView: UIViewController {
+class MemoryUploadView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
     
     var parentView: TimelineViewController!
+    let imagePicker = UIImagePickerController()
+    var image: UIImage?
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        // forces background to darken
+        self.view.backgroundColor = UIColor(white: 0, alpha: 0.6)
+        imagePicker.delegate = self
+        descriptionTextView.delegate = self
+    }
     
     @IBAction func dismissView(sender: AnyObject) {
         parentView.dismissViewControllerAnimated(true) {}
     }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
 
-    override func viewDidLoad() {
-        // forces background to darken
-        self.view.backgroundColor = UIColor(white: 0, alpha: 0.6)
+    @IBAction func selectImage(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        self.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func uploadMemory(sender: AnyObject) {
+        
     }
 }
