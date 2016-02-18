0e67a03839869ff1139e804fae1f7ac8cbe071c5
chetaldrich <chetaldrich@gmail.com>
2016-02-18 11:34:02 -0600

    Text fields now navigable through keyboard
    Pressing Next on the first text fields lets you move to the next one,
    and the pressing it on the description dismisses the keyboard.

--- Carleton150/MemoryUploadView.swift
+++ Carleton150/MemoryUploadView.swift
@@ -2,24 +2,50 @@
 //  MemoryUploadView.swift
 //  Carleton150
 
-class MemoryUploadView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+class MemoryUploadView: UIViewController,
+                        UIImagePickerControllerDelegate,
+                        UINavigationControllerDelegate,
+                        UITextViewDelegate,
+                        UITextFieldDelegate {
     
     var parentView: TimelineViewController!
     let imagePicker = UIImagePickerController()
     var image: UIImage?
     
+    @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var titleField: UITextField!
     
     override func viewDidLoad() {
         // forces background to darken
         self.view.backgroundColor = UIColor(white: 0, alpha: 0.6)
+        
+        // set delegates
         imagePicker.delegate = self
+        nameField.delegate = self
+        titleField.delegate = self
         descriptionTextView.delegate = self
+        
+        // set tags for keyboard
+        nameField.tag = 0
+        titleField.tag = 1
     }
     
     @IBAction func dismissView(sender: AnyObject) {
         parentView.dismissViewControllerAnimated(true) {}
     }
+   
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let nextTag: NSInteger = textField.tag + 1
+        
+        if let nextResponder: UIResponder! = textField.superview!.viewWithTag(nextTag){
+            nextResponder.becomeFirstResponder()
+        } else {
+            descriptionTextView.becomeFirstResponder()
+        }
+        return false
+    }
+ 
     
     func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
         if(text == "\n") {
