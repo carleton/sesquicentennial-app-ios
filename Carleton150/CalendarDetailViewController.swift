a33561ddcd7822026050b4dff67d2b61df5bf6c6
chetaldrich <chetaldrich@gmail.com>
2016-05-10 15:10:44 -0500

    Add time, location to detail view
    Additonally, change the layout of the top section of the detail view so
    that it sizes based on the text inside. It looks cleaner in most
    scenarios even though it changes, and guarantees the text doesn't go
    over.

--- Carleton150/CalendarDetailViewController.swift
+++ Carleton150/CalendarDetailViewController.swift
@@ -7,14 +7,22 @@ import Foundation
 class CalendarDetailViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     
     var eventTitle: String!
+    var eventTime: String!
+    var eventLocation: String!
     
     override func viewDidLoad() {
         self.titleLabel.text = self.eventTitle
+        self.timeLabel.text = self.eventTime
+        self.locationLabel.text = self.eventLocation
     }
     
     func setData(calendarCell: CalendarTableCell) {
         self.eventTitle = calendarCell.title
+        self.eventTime = calendarCell.time
+        self.eventLocation = calendarCell.location
     }
 }
