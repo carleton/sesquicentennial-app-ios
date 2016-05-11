c64e35982d505d2ec3f51c26526ab16fc1c9f063
chetaldrich <chetaldrich@gmail.com>
2016-05-11 12:11:56 -0500

    Add no-connection view to Home page.
    If there wasn't an internet connection on the home page, there wasn't a
    fallback page until now. We now inform the user in the event of a lack
    of an internet connection that they won't be able to view the page.

--- Carleton150/InfoViewController.swift
+++ Carleton150/InfoViewController.swift
@@ -2,13 +2,19 @@
 //  InfoViewController.swift
 //  Carleton150
 
-import Foundation
+import Reachability
 
 class InfoViewController: UIViewController {
     
     @IBOutlet weak var webView: UIWebView!
+	var reach: Reachability?
+	var networkMonitor: Reachability!
     
     override func viewDidLoad() {
+        // set up network monitoring
+		let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+		self.networkMonitor = appDelegate.networkMonitor
+        
         Utils.setUpNavigationBar(self)
         self.loadInfo()
     }
@@ -18,9 +24,16 @@ class InfoViewController: UIViewController {
     }
     
     func loadInfo() {
-        let url = NSURL(string: "https://go.carleton.edu/appinfo")
-        let request = NSMutableURLRequest(URL: url!)
-        request.setValue("CarletonSesquicentennialApp 1.0", forHTTPHeaderField: "UserAgent")
-        webView.loadRequest(request)
+        if self.networkMonitor!.isReachableViaWiFi() || self.networkMonitor!.isReachableViaWWAN() {
+            let url = NSURL(string: "https://go.carleton.edu/appinfo")
+            let request = NSMutableURLRequest(URL: url!)
+            request.setValue("CarletonSesquicentennialApp 1.0", forHTTPHeaderField: "UserAgent")
+            webView.loadRequest(request)
+        } else {
+            let url = NSBundle.mainBundle().pathForResource("no-connection", ofType: "html")
+            let requesturl = NSURL(string: url!)
+            let request = NSURLRequest(URL: requesturl!)
+            webView.loadRequest(request)
+        }
     }
 }
